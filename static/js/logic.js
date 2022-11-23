3b2fb044a217e5f8592b4bca5c10e7615c9991c0
Cindy Toussaint <toussaintcindy@gmail.com>
2022-11-23 05:24:17 -0500

    Mapping Lines 2.0

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+
 // Add console.log to check to see if our code is working.
 console.log("working");
 
@@ -16,21 +17,21 @@ console.log("working");
 //     fillColor: 'ffffa1',
 //  }).addTo(map);
 
-// // Create the map object with a center somewhere between LAX and SFO, and a zoom level.
-// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+// Create the map object with a center somewhere between LAX and SFO, and a zoom level.
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// // Coordinates for each point to be used in the line.
-// let line = [
-//     [33.9416, -118.4085],
-//     [37.6213, -122.3790],
-//     [40.7899, -111.9791],
-//     [47.4502, -122.3088]
-//   ];
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
 
-// // Create a polyline using the line coordinates and make the line yellow.
-// L.polyline(line, {
-//     color: "yellow"
-//   }).addTo(map);
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 // Add circle marker
 L.circleMarker([34.0522, -118.2437], {
@@ -47,11 +48,11 @@ L.circleMarker([34.0522, -118.2437], {
 
 // We create the tile layer that will be the background of our map.
 // Change the map style to "satellite-streets-v11."
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
@@ -77,4 +78,3 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
 });
-
