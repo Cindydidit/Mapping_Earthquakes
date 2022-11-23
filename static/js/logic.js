02d9e9516684f36221ee26d5313e3d2f7ebd7cf4
Cindy Toussaint <toussaintcindy@gmail.com>
2022-11-23 05:01:52 -0500

    Commenting out syntax for merge

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,21 +16,21 @@ console.log("working");
 //     fillColor: 'ffffa1',
 //  }).addTo(map);
 
-// Create the map object with a center somewhere between LAX and SFO, and a zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+// // Create the map object with a center somewhere between LAX and SFO, and a zoom level.
+// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// // Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
+// // Create a polyline using the line coordinates and make the line yellow.
+// L.polyline(line, {
+//     color: "yellow"
+//   }).addTo(map);
 
 // Add circle marker
 L.circleMarker([34.0522, -118.2437], {
