f0f67a2390ec34213109e009fc7c37c18826a325
Cindy Toussaint <toussaintcindy@gmail.com>
2022-11-26 03:52:14 -0500

    Adding deliverable 1

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -31,10 +31,13 @@ let baseMaps = {
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 
+let tectonicPlates = new L.LayerGroup();
+
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Tectonic Plates": tectonicPlates,
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -141,7 +144,18 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (tectonicData) {
+    console.log(tectonicData);
+
+    // pass data to geoJSON() layer:
+    L.geoJson(tectonicData, {
+      color: "orange",
+      weight: 2.5,
+    }).addTo(tectonicPlates);
+  
+    // add the tectonic layer group to the map:
+    tectonicPlates.addTo(map);
     
-  });
+   });
+ 
 });
